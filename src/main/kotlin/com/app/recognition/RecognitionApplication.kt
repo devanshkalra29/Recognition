0cf310fd9dfa08c31424100bfa7acc199117b20a
devansh <devanshkalra29@gmail.com>
2023-01-14 15:34:39 -0600

    initial commit

---
+++ src/main/kotlin/com/app/recognition/RecognitionApplication.kt
@@ -0,0 +1,11 @@
+package com.app.recognition
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecognitionApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecognitionApplication>(*args)
+}
---
+++ src/test/kotlin/com/app/recognition/RecognitionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.app.recognition
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecognitionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
