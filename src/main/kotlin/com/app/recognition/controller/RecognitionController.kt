fdb012624e39fd57451bf6f01737fc30dc73ca80
devansh <devanshkalra29@gmail.com>
2023-01-15 12:28:31 -0600

    changes added but endpoint doesn't work

---
+++ src/main/kotlin/com/app/recognition/controller/RecognitionController.kt
@@ -0,0 +1,14 @@
+package com.app.recognition.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class RecognitionController {
+
+    @GetMapping("/hello")
+    fun helloWorld() : String {
+        return "HelloWorld"
+    }
+
+}
