858d6a2113b8be02602ec8061c10b9e343b6a29d
manupadma <padma.manu.docs@gmail.com>
2023-01-15 14:39:38 -0600

    move this

--- src/main/kotlin/com/app/recognition/controller/RecognitionController.kt
+++ src/main/kotlin/com/app/recognition/controller/RecognitionController.kt
@@ -1,4 +1,4 @@
-package com.app.recognition
+package com.app.recognition.controller
 
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
