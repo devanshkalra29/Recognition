9c567054a5ac198c36eccd740cfb9a286f2dee1a
manupadma <padma.manu.docs@gmail.com>
2023-01-18 18:37:31 -0600

    typo and no lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,9 +27,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
-	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
