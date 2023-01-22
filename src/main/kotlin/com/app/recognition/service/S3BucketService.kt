d41ba950413c167a93781a842c45b3ffe6e4b9a5
manupadma <padma.manu.docs@gmail.com>
2023-01-22 15:04:27 -0600

    refactor

--- src/main/kotlin/com/app/recognition/service/S3BucketService.kt
+++ src/main/kotlin/com/app/recognition/service/S3BucketService.kt
@@ -6,7 +6,7 @@ import com.amazonaws.regions.Regions
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.services.s3.model.PutObjectRequest
-import com.app.recognition.controller.RecognitionController
+import com.app.recognition.controller.S3Controller
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -14,27 +14,20 @@ import org.springframework.stereotype.Service
 import java.io.File
 
 @Service
-class S3BucketService {
+class S3BucketService(
+    @Value("\${aws.credentials.accessKey}") val accessKey: String,
+    @Value("\${aws.credentials.secretKey}") val secretKey: String,
+    @Value("\${aws.s3.bucket}") val bucket: String
+) {
+    private val log: Logger = LoggerFactory.getLogger(S3Controller::class.java)
 
-    @Value("\${aws.credentials.accessKey}")
-    lateinit var accessKey : String
-
-    @Value("\${aws.credentials.secretKey}")
-    lateinit var secretKey : String
-
-    @Value("\${aws.s3.bucket}")
-    lateinit var bucket : String
-
-    private val log: Logger = LoggerFactory.getLogger(RecognitionController::class.java)
-
-    fun uploadFile(fileName: String) {
-
+    fun uploadFile(fileName: String) =
         try {
             log.info("starting upload process")
             val credentials = BasicAWSCredentials(accessKey, secretKey)
-             val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard()
+            val s3Client: AmazonS3 = AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.US_EAST_1)
-                 .withCredentials(AWSStaticCredentialsProvider(credentials))
+                .withCredentials(AWSStaticCredentialsProvider(credentials))
                 .build()
 
             val file = File(fileName)
@@ -44,10 +37,10 @@ class S3BucketService {
             s3Client.putObject(putObjectRequest)
 
             log.info("upload complete!!")
-
         } catch (e: Exception) {
             e.printStackTrace()
             log.error("File upload failed", e)
         }
-    }
+
+
 }
